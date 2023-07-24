fd0895bbbd91134d18466bd920afd13f9e7b0600
EYATUPE <eyatuphilipesamu@gmail.com>
2023-07-24 23:58:10 +0300

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,7 +7,8 @@ corresponding to the range of indexes to return in a list
 for those particular pagination parameters.
 Page numbers are 1-indexed, i.e. the first page is page 1"""
 
-
-def index_range(page: int, page_size: int) -> tuple:
-    # start index and an end index that correlates with the range of
-    return (((page - 1) * page_size), page * page_size)
+def index_range(page, page_size):
+    """ this function which retrieves the content on pages"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
